e04cb9bc67324f8a5485b3d2e1c815d1bcbbdd78
argadepp <argadepp@gmail.com>
2025-01-25 16:14:26 +0530

    added docker push

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -7,4 +7,5 @@ def call(String image,String tag,String path){
     sh "echo 'Building docker image'"
     sh "docker build -t ${image}:${tag} ${path}"
     sh "echo 'Image build successfully'"
+    sh "docker push ${image}:${tag}"
 }
