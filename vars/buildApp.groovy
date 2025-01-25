87b9f6051e5c212b1d4e3f5325ab6bc7b5989e39
argadepp <argadepp@gmail.com>
2025-01-25 14:52:58 +0530

    mvn commad added

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -3,4 +3,8 @@ def call(String appName) {
     sh "echo 'Building ${appName}...'"
     // Add build logic, such as Maven or Gradle commands
     sh "echo 'Build successful for ${appName}'"
+
+    sh "mvn clean install"
 }
+
+
